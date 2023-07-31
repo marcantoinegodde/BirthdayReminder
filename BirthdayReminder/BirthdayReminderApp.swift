c3c920757d60dc64030ee47d65f8979ce96a9d28
Marc-Antoine Godde <marc-antoine.godde@student-cs.fr>
2023-07-31 19:58:24 +0200

    :tada: Initial commit

---
+++ BirthdayReminder/BirthdayReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayReminderApp.swift
+//  BirthdayReminder
+//
+//  Created by Marc-Antoine Godde on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
